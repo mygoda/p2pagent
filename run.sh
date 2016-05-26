9e70e0472d6b450f84222f5584f42dd3c220cf8f
Tao-xu <taoxu@163.com>
2016-05-26 18:34:25 +0800

    添加挂在

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 docker build -t xutaomac/flask_agent .
+mkdir /var/tmp/torrent_tmp
 docker run -d --name agent -p 0.0.0.0:5000:5000  -v /var/run/docker.sock:/var/run/docker.sock -v /var/tmp/torrent_tmp:/var/tmp/torrent_tmp xutaomac/flask_agent
