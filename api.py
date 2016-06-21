f752d85b82b6d3174adde2292507a5d5860e247a
Tao-xu <taoxu@163.com>
2016-06-21 15:05:44 +0800

    测试

--- api.py
+++ api.py
@@ -5,7 +5,7 @@ from flask import Flask
 from flask import jsonify
 import os
 from flask import request
-import docker
+
 from celery import Celery
 import subprocess
 from subprocess import PIPE
@@ -22,8 +22,6 @@ celery.conf.update(app.config)
 TOKEN = "TEST"
 
 TRACKER_URL = "http://205.177.85.132/peertracker/mysql/announce.php"
-
-DOCKER_API_URL = "unix:///var/run/docker.sock"
 
 P2P_PORT = 9091
 
@@ -55,70 +53,6 @@ def task_callback(task_id, status, msg):
     return False
 
 
-def stop(client, container_id):
-    """
-        停止指定的容器
-    :param container_id:
-    :return:
-    """
-    client.stop(container_id=container_id)
-    return True
-
-
-def get_docker_client(base_url):
-    """
-        获取 docker 代理
-    :param base_url:
-    :return:
-    """
-    client = docker.Client(base_url=base_url)
-    return client
-
-
-def my_create_host_config(client, port):
-    """
-        配置 host
-    :param client:
-    :return:
-    """
-    return client.create_host_config(binds=["%s:/var/lib/transmission-daemon/downloads" % P2P_HOST_DOWNLOAD_DIR,
-                                            "%s:/var/lib/transmission-daemon/incomplete" % P2P_HOST_INCOMPLETE_DIR],
-                                     port_bindings={port: ("0.0.0.0", port), "64321/udp": 64321, 64321: 64321})
-
-
-def create_test_container(container_name):
-    """
-        创建测试容器
-    :param client:
-    :return:
-    """
-    try:
-        client = get_docker_client(base_url=DOCKER_API_URL)
-        container = client.create_container(image="ubuntu", tty=True, name=container_name)
-        client.start(container=container)
-        return container
-    except Exception as e:
-        print(e)
-
-
-def create_run_docker(container_name, image, password, port):
-    """
-        创建 transmission docker 容器, 返回容器的 id
-    :param kwargs:
-    :return:
-    """
-    try:
-        client = get_docker_client(base_url=DOCKER_API_URL)
-        config = my_create_host_config(client=client, port=port)
-        container = client.create_container(image=image, name=container_name, ports=[64321, (64321, "udp"), port],
-                                            stdin_open=False, tty=False, environment={"ADMIN_PASS": password},
-                                            host_config=config)
-
-        client.start(container=container)
-
-        return container["Id"]
-    except Exception as e:
-        print(str(e))
 
 
 @celery.task
@@ -142,14 +76,7 @@ def create_torrent(path, name, comment, task_id):
         task_callback(task_id=task_id, status="ERROR", msg=str(e))
 
 
-@app.route('/', methods=["POST"])
-def hello_world():
-    dic = request.form
-    name = dic.get("name")
-    container_id = create_test_container(container_name=name)
-    dic = {"id": container_id}
 
-    return jsonify(**dic)
 
 
 @app.route('/test/', methods=["POST"])
@@ -187,45 +114,6 @@ def torrents():
 
             return jsonify(**result)
 
-
-@app.route("/containers/", methods=["POST", "PUT"])
-def containers():
-    """
-        创建容器
-    :return:
-    """
-    result = {"status": "ok", "msg": "do it"}
-    if request.method == "POST":
-        data = request.form
-        token = data.get("token", "")
-        if token == TOKEN:
-            # 正常的请求
-            container_name = data.get("container_name", "")
-            image = data.get("image", "")
-            password = data.get("password", "")
-            port = data.get("port", 9091)
-            container_id = create_run_docker(container_name=container_name, password=password, image=image, port=port)
-            result["data"] = container_id
-            return jsonify(**result)
-
-        else:
-            # 非正常的请求
-            result["status"] = "error"
-            result["msg"] = "you are forbid"
-            result["data"] = {}
-
-            return jsonify(**result)
-
-    if request.method == "PUT":
-        # 目前支持 关闭 操作
-        data = request.form
-        action = data.get("action", "stop")
-        container_id = data.get("container_id", "")
-        if action == "stop":
-            client = get_docker_client(base_url=DOCKER_API_URL)
-            stop(client=client, container_id=container_id)
-            result["data"] = {}
-            return jsonify(**result)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
