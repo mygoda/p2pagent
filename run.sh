1b9da123fbbf35d71e93aedca8c176a08d86e8d3
Tao-xu <taoxu@163.com>
2016-06-22 21:05:16 +0800

    add nginx conf

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gunicorn -w 4 -b 0.0.0.0:5046 wsgi:app --log-file error.log&
+gunicorn -w 4 -b 127.0.0.1:5046 wsgi:app --log-file error.log&
 
 export C_FORCE_ROOT=true
 celery worker -A api.celery -w 4 --loglevel=info
