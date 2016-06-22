598754c30038967c77e8c7042f4e364fb736db2d
Tao-xu <taoxu@163.com>
2016-06-22 20:59:55 +0800

    xiugai

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-gunicorn -w 4 -b 0.0.0.0:5045 wsgi:app --log-file error.log
+gunicorn -w 4 -b 0.0.0.0:5046 wsgi:app --log-file error.log&
+
 export C_FORCE_ROOT=true
-celery worker -A api.celery --loglevel=info
+celery worker -A api.celery -w 4 --loglevel=info
